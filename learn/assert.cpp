aa2c3ac4531dc97eb6867653b71f2bc4ced09ea3
ki <ki@ki.ki>
2018-02-19 18:08:34 +0800

    s

---
+++ learn/assert.cpp
@@ -0,0 +1,15 @@
+//assert
+
+#include <assert.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+cout<<"begin"<<endl;
+assert(0);
+cout<<"return";
+
+
+}
---
+++ learn/make/include/test.h
@@ -0,0 +1,20 @@
+
+
+#ifndef test_h
+#define test_h
+
+
+#include <string>
+#include <iostream>
+#include "test.h"
+using namespace std;
+
+
+
+
+void say(string);
+
+
+
+ 
+#endif
