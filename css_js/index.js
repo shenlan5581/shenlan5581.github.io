3e01ac01af9cbf716db81c2999befd0628d3a870
ki <ki@ki.ki>
2018-03-27 05:27:14 +0800

    web

--- css_js/index.js
+++ css_js/index.js
@@ -1,16 +1,17 @@
 
-
-//document.getElementById("s").onclick = show();
 var stats=0;
-function show(x)
-{   if(stats == 0){
-      document.getElementById(x).style.display = "block";
-      stats =1;
-    }else{
-        document.getElementById(x).style.display = "none";
-        stats =0;
-    }  
-}
+ function show(){   
+          if(stats == 0){
+          document.getElementById('x').style.display = "block";
+          stats =1;
+        }else{
+          document.getElementById('x').style.display = "none";
+            stats =0;
+        }  
+ };
+ code =  document.getElementById("s");
+ code.onmousemove = show;
 
+ 
  
  
